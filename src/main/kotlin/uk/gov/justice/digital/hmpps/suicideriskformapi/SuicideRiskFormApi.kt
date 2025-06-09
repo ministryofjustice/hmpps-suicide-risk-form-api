b97194244dfbef4120f33bc79bfb7edce55d6350
runner <runner@hmpps-github-actions-runner-784c475d96-db6b8>
2025-06-09 14:10:56 +0000

    Rename project

--- src/main/kotlin/uk/gov/justice/digital/hmpps/suicideriskformapi/SuicideRiskFormApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/suicideriskformapi/SuicideRiskFormApi.kt
@@ -1,11 +1,11 @@
-package uk.gov.justice.digital.hmpps.templatepackagename
+package uk.gov.justice.digital.hmpps.suicideriskformapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class HmppsTemplateKotlin
+class SuicideRiskFormApi
 
 fun main(args: Array<String>) {
-  runApplication<HmppsTemplateKotlin>(*args)
+  runApplication<SuicideRiskFormApi>(*args)
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/suicideriskformapi/integration/NotFoundTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/suicideriskformapi/integration/NotFoundTest.kt
@@ -1,4 +1,4 @@
-package uk.gov.justice.digital.hmpps.templatepackagename.integration
+package uk.gov.justice.digital.hmpps.suicideriskformapi.integration
 
 import org.junit.jupiter.api.Test
 
