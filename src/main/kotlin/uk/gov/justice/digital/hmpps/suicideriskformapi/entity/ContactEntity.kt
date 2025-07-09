180a32e8b1b8a35d8d09179e28e7b7988cdf54fc
Peter Wilson <40563223+peter-bcl@users.noreply.github.com>
2025-07-09 14:57:47 +0100

    SRF-20 Initial Project Setup

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/suicideriskformapi/entity/ContactEntity.kt
@@ -0,0 +1,42 @@
+package uk.gov.justice.digital.hmpps.suicideriskformapi.entity
+
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Entity
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToOne
+import jakarta.persistence.Table
+import org.springframework.data.annotation.CreatedBy
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+import java.util.*
+
+@Entity
+@Table(name = "contact")
+@EntityListeners(AuditingEntityListener::class)
+data class ContactEntity(
+  @Id
+  val id: UUID = UUID.randomUUID(),
+  @JoinColumn(name = "suicide_risk_id")
+  @ManyToOne
+  var suicideRisk: SuicideRiskEntity? = null,
+  val contactTypeDescription: String? = null,
+  val contactPerson: String? = null,
+  @OneToOne(cascade = [CascadeType.ALL], orphanRemoval = true)
+  @JoinColumn(name = "contact_location_id", unique = true)
+  val contactLocation: AddressEntity? = null,
+  val formSent: Boolean? = null,
+  @CreatedBy
+  var createdByUser: String? = null,
+  @CreatedDate
+  var createdDatetime: LocalDateTime? = null,
+  @LastModifiedDate
+  var lastUpdatedDatetime: LocalDateTime? = null,
+  @LastModifiedBy
+  var lastUpdatedUser: String? = null,
+)
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/suicideriskformapi/model/CreateResponse.kt
@@ -0,0 +1,5 @@
+package uk.gov.justice.digital.hmpps.suicideriskformapi.model
+
+import java.util.*
+
+data class CreateResponse(val id: UUID, val url: String)
