85f153f20056fc86c122d17b3cb3e6f53394ffbc
Joseph.Dundon <jdundon@unilink.com>
2025-09-25 13:51:51 +0100

    ktlint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/suicideriskformapi/controller/SuicideRiskController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/suicideriskformapi/controller/SuicideRiskController.kt
@@ -11,6 +11,7 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
