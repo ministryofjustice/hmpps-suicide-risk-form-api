e443fa59a476e0c789d825960a2b50f4939dc99b
Joseph.Dundon <jdundon@unilink.com>
2026-05-05 15:09:39 +0100

    SRF-196 Move NSI; REVIEW_REQUIRED_DATE set 1 hour before actual event timestamp - Added jackson config to prevent Jackson normalising ZonedDateTime to UTC for SQS events

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/suicideriskformapi/config/JacksonConfig.kt
@@ -0,0 +1,18 @@
+package uk.gov.justice.digital.hmpps.suicideriskformapi.config
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Bean
+
+@Configuration
+class JacksonConfig {
+
+  @Bean
+  fun jacksonCustomizer(): Jackson2ObjectMapperBuilderCustomizer =
+    Jackson2ObjectMapperBuilderCustomizer { builder ->
+      builder.featuresToDisable(
+        DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE
+      )
+    }
+}
