8fd32e3195c0fb2a81e864fb2db3a2fa24f560d9
Rob McCormack <56261775+robertmccormackbconline@users.noreply.github.com>
2026-08-14 09:48:26 +0100

    SRF-255: Address Length changes (#102)

---
+++ src/main/resources/db/migration/V1_10__address_field_lengths.sql
@@ -0,0 +1,8 @@
+ALTER TABLE public.address
+    ALTER COLUMN building_name TYPE varchar(80);
+
+ALTER TABLE public.address
+    ALTER COLUMN street_name TYPE varchar(80);
+
+ALTER TABLE public.address
+    ALTER COLUMN district TYPE varchar(80);
