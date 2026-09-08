1cee128e6e8ce61f4c4de2b3c0b9a0562718910e
peter wilson <pwilson@unilink.com>
2026-09-08 15:42:40 +0100

    Major Dependency Update

--- src/main/kotlin/uk/gov/justice/digital/hmpps/suicideriskformapi/service/SuicideRiskService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/suicideriskformapi/service/SuicideRiskService.kt
@@ -16,6 +16,7 @@ import uk.gov.justice.digital.hmpps.suicideriskformapi.model.InitialiseSuicideRi
 import uk.gov.justice.digital.hmpps.suicideriskformapi.model.SuicideRisk
 import uk.gov.justice.digital.hmpps.suicideriskformapi.repository.ContactRepository
 import uk.gov.justice.digital.hmpps.suicideriskformapi.repository.SuicideRiskRepository
+import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.util.*
 
