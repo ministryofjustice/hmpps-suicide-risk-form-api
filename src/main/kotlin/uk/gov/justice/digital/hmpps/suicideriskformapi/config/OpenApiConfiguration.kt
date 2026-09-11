7deb7d93a08968ec138174e130dac9acf8ec9a99
Peter Wilson <40563223+peter-bcl@users.noreply.github.com>
2026-09-11 15:14:36 +0100

    open ai config update (#120)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/suicideriskformapi/config/OpenApiConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/suicideriskformapi/config/OpenApiConfiguration.kt
@@ -1,8 +1,10 @@
 package uk.gov.justice.digital.hmpps.suicideriskformapi.config
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
 import io.swagger.v3.oas.models.servers.Server
 import org.springframework.boot.info.BuildProperties
@@ -30,7 +32,13 @@ class OpenApiConfiguration(buildProperties: BuildProperties) {
       Info().title("HMPPS Suicide Risk Form Api").version(version)
         .contact(Contact().name("HMPPS Digital Studio").email("feedback@digital.justice.gov.uk")),
     )
-  // TODO Add security schema and roles in `.components()` and `.addSecurityItem()`
+    .components(
+      Components().addSecuritySchemes(
+        "suicide-risk-api-ui-role",
+        SecurityScheme().addBearerJwtRequirement("ROLE_SUICIDE_RISK"),
+      ),
+    )
+    .addSecurityItem(SecurityRequirement().addList("suicide-risk-api-ui-role", listOf("read")))
 }
 
 private fun SecurityScheme.addBearerJwtRequirement(role: String): SecurityScheme = type(SecurityScheme.Type.HTTP)
